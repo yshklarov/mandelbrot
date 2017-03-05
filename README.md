50f6e2b8cd767fd2c46d519d6d284910c3db4bc7
Yakov Shklarov <yshklarov@gmail.com>
2017-03-05 13:47:19 -0800

    Switch from threads to processes for snappier GUI

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ Mandelbrot
 
 This is a simple desktop tool for exploring the Mandelbrot set.
 
+Usage:
+
+- Scroll to zoom in/out.
+
+Screenshot:
+
 ![screenshot](docs/screenshot.png)
--- mandelbrot/main.py
+++ mandelbrot/main.py
@@ -4,6 +4,8 @@ import tkinter as tk
 import pygame
 import os
 import sys
+import multiprocessing as mp
+import queue
 import threading
 
 import mandelbrot
@@ -18,31 +20,30 @@ class Viewport:
 
     # The window referenced by window_id must exist (eg. call root.update() in tkinter)
     def __init__(self, window_id):
-        os.environ['SDL_WINDOWID'] = str(window_id)
-        self.canvas = pygame.display.set_mode()
-        pygame.display.init()
-
         self.width, self.height = 0, 0
         self.re_min, self.re_max = -2, 0.8
         self.im_min, self.im_max = -1.4, 1.4
 
-        self.render_sem = threading.BoundedSemaphore(1)
-        self.render_sem.acquire()
-        self.rendering_sem = threading.BoundedSemaphore(1)
-        self.rendering_sem.acquire()
-        self.redraw_sem = threading.Semaphore(1)
+        self.redraw_sem = mp.Semaphore()
         self.redraw_waiting = False
+
+        self.render_event = mp.Event()
+        self.refresh_event = mp.Event()
+        self.stop_event = mp.Event()
+        self.quit_event = mp.Event()
+        self.size_q = mp.Queue()
+        self.bounds_q = mp.Queue()
 
         self.status_callbacks = []
 
-        rt = threading.Thread(target=self.render)
-        rt.daemon = True  # TODO: a more graceful exit
-        rt.start()
+        self.render_p = mp.Process(target=self.render, daemon=True, args=[window_id])
+        self.render_p.start()
 
     def set_size(self, width, height):
         if (self.width, self.height) != (width, height):
             self.width = width
             self.height = height
+            self.size_q.put((self.width, self.height))
             self.redraw_delayed(0.100)
         self.update_status()
 
@@ -70,7 +71,7 @@ class Viewport:
         if x is None:
             x = self.width // 2
         if y is None:
-            y = self.width // 2
+            y = self.height // 2
 
         # Center of dilation
         center_re = self.re_min + (x / self.width) * (self.re_max - self.re_min)
@@ -80,42 +81,72 @@ class Viewport:
         self.re_max = center_re + ratio*(self.re_max - center_re)
         self.im_min = center_im - ratio*(center_im - self.im_min)
         self.im_max = center_im + ratio*(self.im_max - center_im)
+
+        self.bounds_q.put((self.re_min, self.re_max, self.im_min, self.im_max))
         self.update_status()
         self.redraw()
 
     # Re-paint the window surface (without rendering anew)
     def refresh(self):
-        pygame.display.update()
+        self.refresh_event.set()
 
-    # Redraw after the given delay (in seconds). Repeat calls will reset the delay. Do not block.
+    def refresh_watchdog(self):
+        while self.refresh_event.wait():
+            self.refresh_event.clear()
+            pygame.display.update()
+
+    def close(self):
+        self.quit_event.set()
+        self.stop_event.set()
+        self.render_event.set()  # Release block in render()
+
+    # Call redraw() after the given delay (in seconds). Repeat calls will reset the delay. Do not block.
     def redraw_delayed(self, delay):
-        def wait():
-            # Wait for timeout
-            while self.redraw_sem.acquire(timeout=delay):
-                pass
-            self.redraw_waiting = False
-            self.redraw()
-        self.redraw_sem.release()
         if not self.redraw_waiting:
+            def wait():
+                # Wait until 'delay' seconds after the last acquire
+                while self.redraw_sem.acquire(timeout=delay):
+                    pass
+                self.redraw_waiting = False
+                self.redraw()
             self.redraw_waiting = True
-            t = threading.Thread(target=wait)
-            t.daemon = True
+            t = threading.Thread(target=wait, daemon=True)
             t.start()
+        self.redraw_sem.release()
 
     # Re-render immediately. Block until rendering has begun anew.
     def redraw(self):
-        self.stop = True
-        self.render_sem.release()
-        self.rendering_sem.acquire()
-    
-    def render(self):
-        while True:
-            self.render_sem.acquire()
-            self.stop = False
-            self.rendering_sem.release()
+        self.stop_event.set()
+        self.render_event.set()
+
+    def render(self, window_id):
+        os.environ['SDL_WINDOWID'] = str(window_id)
+        self.canvas = pygame.display.set_mode()
+        pygame.display.init()
+        threading.Thread(target=self.refresh_watchdog, daemon=True).start()
+
+        while not self.quit_event.is_set():
+            self.render_event.wait()
+            self.render_event.clear()
+            self.stop_event.clear()
+
+            # Get new size if changed
+            try:
+                while True:
+                    self.width, self.height = self.size_q.get_nowait()
+            except queue.Empty as e: pass
+
+            # Get new bounds if changed
+            try:
+                while True:
+                    self.re_min, self.re_max, self.im_min, self.im_max = \
+                            self.bounds_q.get_nowait()
+            except queue.Empty as e: pass
+
             self.canvas.fill((0, 0, 0), pygame.Rect(0, 0, self.width, self.height))
+
             for x in range(0, self.width):
-                if self.stop:
+                if self.stop_event.is_set() or self.quit_event.is_set():
                     break
                 re = self.re_min + ( (self.re_max - self.re_min) * x / self.width )
                 for y in range(0, self.height):
@@ -130,6 +161,8 @@ def widget_size(widget):
 
 
 if __name__ == "__main__":
+    mp.set_start_method('spawn')
+
     root = tk.Tk()
     root.title(PROGRAM_NAME)
     root.geometry('{}x{}'.format(WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -146,7 +179,6 @@ if __name__ == "__main__":
     status = tk.StringVar()
     status_bar = tk.Label(root, bd=1, relief=tk.SUNKEN, anchor=tk.W, height=1, textvariable=status)
     status_bar.pack(fill=tk.X)
-
 
     file_menu = tk.Menu(menu_bar, tearoff=0)
     view_menu = tk.Menu(menu_bar, tearoff=0)
@@ -166,16 +198,19 @@ if __name__ == "__main__":
     root.bind_all('r', lambda _: viewport.redraw())
     root.bind_all('<Key-F1>', lambda _: status.set('Drag to move; scroll to zoom'))
     root.bind_all('<KP_Add>', lambda _: viewport.zoom_in())
+    root.bind_all('<plus>', lambda _: viewport.zoom_in())
     root.bind_all('<KP_Subtract>', lambda _: viewport.zoom_out())
-
+    root.bind_all('<minus>', lambda _: viewport.zoom_out())
     embed.bind('<Button-4>', lambda ev: viewport.zoom_in(x=ev.x, y=ev.y))
     embed.bind('<Button-5>', lambda ev: viewport.zoom_out(x=ev.x, y=ev.y))
 
-    # Window resize
-    root.bind('<Configure>', lambda _: viewport.set_size(*widget_size(embed)))
+    # <Configure> gets called on widget resize
+    embed.bind('<Configure>', lambda _: viewport.set_size(*widget_size(embed)))
     embed.bind('<Visibility>', lambda _: embed.after(1, viewport.refresh))
 
-    viewport.register_status_callback(status.set)
     viewport.set_size(*widget_size(embed))
+    viewport.register_status_callback(status.set)
 
     root.mainloop()
+
+    viewport.close()
